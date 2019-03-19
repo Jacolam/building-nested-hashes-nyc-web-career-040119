c91c97268cf83a796c3e405f4e1ecbbdaa0e0540
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-19 17:53:26 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch,
-      :matriarch,
+      :patriarch ,
+      :matriarch ,
       :hero ,
       :hero_friends
       }, 
     :capulet => {
-     :patriarch,
-     :matriarch,
-     :heroine,
+     :patriarch ,
+     :matriarch ,
+     :heroine ,
      :heroine_friends
       }}
 
